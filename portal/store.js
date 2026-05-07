ea400a7c86899e9125f41de0094d78727cb1dca2
mabercor <manbercor@hotmail.com>
2026-05-07 13:06:44 -0400

    Display welcome message based on URL client parameter

--- portal/store.js
+++ portal/store.js
@@ -1,1 +1,11 @@
+const params = new URLSearchParams(window.location.search);
 
+const client = params.get("client");
+
+const welcome = document.getElementById("clientWelcome");
+
+if (client && welcome) {
+
+  welcome.innerText = `Welcome ${client}`;
+
+}
